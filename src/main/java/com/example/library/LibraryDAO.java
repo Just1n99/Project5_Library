2f1cdc3f3a8463c9e0fa0349767805e2aec02130
seonghwan <hoanshim01@handong.ac.kr>
2023-12-15 16:02:34 +0900

    Update Library server

--- src/main/java/com/example/library/LibraryDAO.java
+++ src/main/java/com/example/library/LibraryDAO.java
@@ -13,14 +13,8 @@ public class LibraryDAO {
     // 책을 관리하는 페이지로 만들 것인지 - 이름, 작가, 출판사, 분류, 등록 날짜,
     // 책을 대여하는 페이지로 만들 것인지 - 이름, 작가, 출판사, 분류, 빌린 사람, 빌려갔는지 안빌려갔는지, 반납 일자.
     public int insertLibrary(LibraryVO vo){
-        String sql = "insert into Library (name, writer, publisher, classification, borrow, borrower) values("
-                +"'"+vo.getName()+"',"
-                +"'"+vo.getWriter()+"',"
-                +"'"+vo.getPublisher()+"',"
-                +"'"+vo.getClassification()+"',"
-                +"'"+vo.getBorrow()+"',"
-                +"'"+vo.getBorrower()+"',";
-        return sqlSession.update(sql);
+        int result = sqlSession.update("Library.insertLibrary", vo)
+        return result;
     }
 
     public int deleteLibrary(int bookID){
